65d15923cc55b14ba98329e99fc6dba1d92a0337
Eric Sowell <eric.sowell@gmail.com>
2017-08-17 10:14:10 -0500

    logging process.env

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.static('public'));
 
+console.log('=========================');
+console.log(process.env);
 
 app.listen(7990, () => {
   console.log('Listening on port 7990');
